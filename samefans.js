bc8c00b5b94859f53ee53e7699e454e4cf24c199
Wei Guangcheng <twksos@qq.com>
2012-09-05 21:14:10 +0800

    format percentage and add names.

--- samefans.js
+++ samefans.js
@@ -15,19 +15,24 @@ function loadFans(userNameInput){
     }
   });
 }
+function percentage(n){
+  return (n*100).toFixed(2)+"%";
+} 
 function calcIntersection(){
     var user0Ids = JSON.parse($('#user-0-name').attr('data-fans')).ids;
     var user1Ids = JSON.parse($('#user-1-name').attr('data-fans')).ids;
     var intersection = _.intersection(user0Ids, user1Ids);
-    $('#user-0-fans').text(user0Ids.length)
-    $('#user-1-fans').text(user1Ids.length)
-    $('#common-fans').text(intersection.length)
-    $('#common-fans-user-0-percentage').text(intersection.length/user0Ids.length)
-    $('#common-fans-user-1-percentage').text(intersection.length/user1Ids.length)
+    $('#user-0-fans').text(user0Ids.length);
+    $('#user-1-fans').text(user1Ids.length);
+    $('#common-fans').text(intersection.length);
+    $('#common-fans-user-0-percentage').text(percentage(intersection.length/user0Ids.length));
+    $('#common-fans-user-1-percentage').text(percentage(intersection.length/user1Ids.length));
 }
 $(function (){
   _.after(1, calcIntersection);
   $('#load').click(function(e){
+    $('.user-0-name').text($('#user-0-name').val());
+    $('.user-1-name').text($('#user-1-name').val());
     loadFans('#user-0-name');
     loadFans('#user-1-name');
     e.preventDefault();
