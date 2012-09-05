3a015afc9ca36a6812ba38800214b0ed0bff7358
Wei Guangcheng <twksos@qq.com>
2012-09-05 20:46:33 +0800

    function done.

--- index.php
+++ index.php
@@ -21,20 +21,36 @@ $code_url = $o->getAuthorizeURL( WB_CALLBACK_URL );
 function loadFans(userNameInput){
   $.ajax({
     url: "http://weiboapp.twksos.com/followers_ids.php?name=" + $(userNameInput).val(),
+    async: false
   }).done(function(data) {
-    console.log(data);
     fans = JSON.parse(data);
+    console.log(fans);
+    if(fans.error){
+      alert('错误：'+fans.error);
+    }
     if(fans.next_cursor == 0){
-      $(userNameInput).attr('data-fans',fans);
+      $(userNameInput).attr('data-fans',data);
+      calcIntersection();
     }else {
       alert('暂不支持5000粉丝以上的用户。');
     }
   });
 }
+function calcIntersection(){
+    var user0Ids = JSON.parse($('#user-0-name').attr('data-fans')).ids;
+    var user1Ids = JSON.parse($('#user-1-name').attr('data-fans')).ids;
+    var intersection = _.intersection(user0Ids, user1Ids);
+    $('#user-0-fans').text(user0Ids.length)
+    $('#user-1-fans').text(user1Ids.length)
+    $('#common-fans').text(intersection.length)
+    $('#common-fans-user-0-percentage').text(intersection.length/user0Ids.length)
+    $('#common-fans-user-1-percentage').text(intersection.length/user1Ids.length)
+}
 $(function (){
+  _.after(1, calcIntersection);
   $('#load').click(function(){
-    loadFans('#user-name-0');
-    loadFans('#user-name-1');
+    loadFans('#user-0-name');
+    loadFans('#user-1-name');
   });
 });
 </script>
