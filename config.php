f7aa502a1dd89bc980ecf134b8cea40afdada27c
Wei Guangcheng <twksos@qq.com>
2012-10-12 21:15:12 +0800

    hide config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=UTF-8');
-
-define( "WB_AKEY" , '1678227100' );
-define( "WB_SKEY" , '6eef21e3860c6c03af4482a1353ca10c' );
-define( "WB_CALLBACK_URL" , 'http://weiboapp.twksos.com/callback.php' );
